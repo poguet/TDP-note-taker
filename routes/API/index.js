ce80bcce280ca440f8378c10e25041da63dd5d45
poguet <pogue12@ymail.com>
2023-03-05 11:03:59 -0600

    added routes and files

---
+++ routes/API/index.js
@@ -0,0 +1,16 @@
+const router = require('express').Router()
+const db = require('../../db/db.json')
+
+router.get('/', (req,res)=>{
+
+}) 
+
+router.post('/', (req,res)=>{
+
+})
+
+router.delete('/:id', (req,res)=>{
+    
+})
+
+module.exports = router
---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+// Helper method for generating unique ids
+const routes = require('./routes')
+
+const PORT = 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+app.use(routes);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
